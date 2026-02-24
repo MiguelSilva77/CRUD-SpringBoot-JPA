379b7fd95b74cac09ed99225e0ca14f2194e59cc
Miguel Silva <miguelmartins.migas@gmail.com>
2026-02-23 22:26:35 -0300

    create class category

--- course/src/main/java/com/edu/course/config/TestConfig.java
+++ course/src/main/java/com/edu/course/config/TestConfig.java
@@ -10,9 +10,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.edu.course.entities.Category;
 import com.edu.course.entities.Order;
 import com.edu.course.entities.User;
 import com.edu.course.entities.enums.OrderStatus;
+import com.edu.course.repositories.CategoryRepository;
 import com.edu.course.repositories.OrderRepository;
 import com.edu.course.repositories.UserRepository;
 
@@ -25,6 +27,9 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private OrderRepository orderrepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -35,9 +40,14 @@ public class TestConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITTING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITTING_PAYMENT, u1); 
-
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		userrepository.saveAllAndFlush(Arrays.asList(u1, u2));
 		orderrepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 	}
 
